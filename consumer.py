4d479685c05f2907ea32aa02bcfcea3c944a6707
Luke <lucky.thinkaxis@gmail.com>
2022-02-27 15:14:24 +0700

    Config sever

--- consumer.py
+++ consumer.py
@@ -1,13 +1,27 @@
+import os
 import json 
 from kafka import KafkaConsumer
+# from dotenv import load_dotenv
 
+# load_dotenv()
+# BOOTSTRAP_SERVERS=os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+# TOPIC_NAME="MSKTutorialTopic"
+# SASL_USERNAME=os.getenv("KAFKA_SASL_USERNAME")
+# SASL_PASSWORD=os.getenv("KAFKA_SASL_PASSWORD")
+
+BOOTSTRAP_SERVERS="b-3-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196,b-1-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196,b-2-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196".split(",")
+TOPIC_NAME="MSKTutorialTopic"
+SASL_USERNAME="iotproject"
+SASL_PASSWORD="iotproject"
 
 if __name__ == '__main__':
     # Kafka Consumer 
-    consumer = KafkaConsumer(
-        'messages',
-        bootstrap_servers='localhost:9092',
-        auto_offset_reset='earliest'
-    )
+    # consumer = KafkaConsumer(
+    #     'messages',
+    #     bootstrap_servers='localhost:9092',
+    #     auto_offset_reset='earliest'
+    # )
+    consumer = KafkaConsumer(security_protocol="SASL_SSL",  sasl_mechanism="SCRAM-SHA-512", sasl_plain_username=SASL_USERNAME, sasl_plain_password=SASL_PASSWORD, bootstrap_servers=BOOTSTRAP_SERVERS)
+    consumer.subscribe(TOPIC_NAME)
     for message in consumer:
         print(json.loads(message.value))
--- producer.py
+++ producer.py
@@ -1,21 +1,36 @@
+import os
 import time 
 import json 
 import random 
 from datetime import datetime
 from data_generator import generate_message
 from kafka import KafkaProducer
+# from dotenv import load_dotenv
 
+# load_dotenv()
+# BOOTSTRAP_SERVERS=os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+# TOPIC_NAME="MSKTutorialTopic"
+# SASL_USERNAME=os.getenv("KAFKA_SASL_USERNAME")
+# SASL_PASSWORD=os.getenv("KAFKA_SASL_PASSWORD")
 
+BOOTSTRAP_SERVERS="b-3-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196,b-1-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196,b-2-public.iot-project.dztnn7.c3.kafka.ap-southeast-1.amazonaws.com:9196".split(",")
+TOPIC_NAME="MSKTutorialTopic"
+SASL_USERNAME="iotproject"
+SASL_PASSWORD="iotproject"
 # Messages will be serialized as JSON 
 def serializer(message):
     return json.dumps(message).encode('utf-8')
 
 
 # Kafka Producer
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=serializer
-)
+# producer = KafkaProducer(
+#     bootstrap_servers=['localhost:9092'],
+#     value_serializer=serializer,
+#     compression_type='gzip'
+# )
+
+producer = KafkaProducer(value_serializer=serializer, security_protocol="SASL_SSL",  sasl_mechanism="SCRAM-SHA-512", sasl_plain_username=SASL_USERNAME, sasl_plain_password=SASL_PASSWORD, bootstrap_servers=BOOTSTRAP_SERVERS)
+
 
 
 if __name__ == '__main__':
@@ -26,8 +41,6 @@ if __name__ == '__main__':
         
         # Send it to our 'messages' topic
         print(f'Producing message @ {datetime.now()} | Message = {str(dummy_message)}')
-        producer.send('messages', dummy_message)
-        
-        # Sleep for a random number of seconds
-        time_to_sleep = random.randint(1, 11)
-        time.sleep(time_to_sleep)
+        test = producer.send(TOPIC_NAME, dummy_message)
+        print(test)
+        time.sleep(10)
